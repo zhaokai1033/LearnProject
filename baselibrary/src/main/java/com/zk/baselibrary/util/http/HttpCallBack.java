56dcb3e0601810453a48bb8d3e4cf6aeb3a03c5e
zhaokai1033 <zhaokai1033@126.com>
2017-03-20 13:41:24 +0800

    Perfect network request tool

---
+++ baselibrary/src/main/java/com/zk/baselibrary/util/http/HttpCallBack.java
@@ -0,0 +1,12 @@
+package com.zk.baselibrary.util.http;
+
+/**
+ * ================================================
+ * Created by zhaokai on 2017/3/17.
+ * Email zhaokai1033@126.com
+ * Describe :
+ * ================================================
+ */
+
+public interface HttpCallBack {
+}
